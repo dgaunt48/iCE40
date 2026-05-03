29925cf6d887af3ccec5a0ab3e3aac08fc1e879c
Dave Gaunt <dgaunt48@gmail.com>
2026-05-03 21:24:26 +1000

    - Added IRQ Pin And Timer 1 Interrupt.

--- VIA_6522/VIA_6522_tb.v
+++ VIA_6522/VIA_6522_tb.v
@@ -23,10 +23,11 @@ reg bCS_n = 1;
 reg bRead = 1;
 reg [3:0] nRS = 0;
 
-reg [7:0] mWriteData = 0;
+reg [7:0] nWriteData = 0;
 wire [7:0] nData;
+wire bIRQ_n;
 
-assign nData = bRead ? 8'bz : mWriteData;
+assign nData = bRead ? 8'bz : nWriteData;
 
 VIA_6522 UUT (
 	.clk(clk),
@@ -35,7 +36,8 @@ VIA_6522 UUT (
 	.bCS_n(bCS_n),
 	.bRead(bRead),
 	.nRS(nRS[3:0]),
-	.nData(nData[7:0])
+	.nData(nData[7:0]),
+	.bIRQ_n(bIRQ_n)
 );
 
 initial begin
@@ -47,41 +49,49 @@ initial begin
 
 	#2 bCS_n = 0;					// Enable Interrupts VIA_IER_CA2, VIA_IER_SR and VIA_IER_T1 0x45
 	nRS = VIA_REG_IER;
-	mWriteData = 8'hC5;
+	nWriteData = 8'hC5;
 	#1 bRead = 0;
 	#6 bRead = 1;
 	bCS_n = 1;
 
 	#2 bCS_n = 0;					// Disable Interrupts VIA_IER_CA2, VIA_IER_SR 0x40
 	nRS = VIA_REG_IER;
-	mWriteData = 8'h05;
+	nWriteData = 8'h05;
 	#1 bRead = 0;
 	#6 bRead = 1;
 	bCS_n = 1;
 
 	#2 bCS_n = 0;					// Write 0x40 To ACR Putting Timer 1 Into Free Running Mode
 	nRS = VIA_REG_ACR;
-	mWriteData = 8'h40;
+	nWriteData = 8'h40;
 	#1 bRead = 0;
 	#6 bRead = 1;
 	bCS_n = 1;
 
 	#2 bCS_n = 0;					// Write 0x26 To Timer 1 Low Order Latch Through Count Register
 	nRS = VIA_REG_T1CL;
-	mWriteData = 8'h06;		// 26
+	nWriteData = 8'h06;		// 26
 	#1 bRead = 0;
 	#6 bRead = 1;
 	bCS_n = 1;
 
 	#2 bCS_n = 0;					// Write 0x48 To Timer 1 High Order Latch And Start The Timer
 	nRS = VIA_REG_T1CH;
-	mWriteData = 8'h00;		// 48
+	nWriteData = 8'h00;		// 48
 	#1 bRead = 0;
 	#6 bRead = 1;
 	bCS_n = 1;
 
-	#2 bCS_n = 0;
-	nRS = VIA_REG_T1CH;
+	#65								// Read Interrupt Flags Register
+	nRS = VIA_REG_IFR;
+	#1 bCS_n = 0;
+	#5 bCS_n = 1;
+
+
+	nWriteData = 8'h40;
+	#2 bRead = 0;
+	bCS_n = 0;
+	#5 bCS_n = 1;
 
 
 	#(DURATION) $display("End of simulation");

fd5c8f1dc811ca187be99396d1a43721bd773d3e
Dave Gaunt <dgaunt48@gmail.com>
2026-05-15 02:04:38 +1000

    - Added Missing Read IER Register.

--- VIA_6522/VIA_6522_tb.v
+++ VIA_6522/VIA_6522_tb.v
@@ -16,8 +16,8 @@ module VIA_6522_TB();
 
 parameter DURATION = 100;
 
-reg clk = 0;
-always #4.5 clk = ~clk;				// 902.1966ns
+reg bPhase2Clock = 0;
+always #8 bPhase2Clock = ~bPhase2Clock;
 
 reg bReset_n = 0;
 reg bCS = 0;
@@ -32,7 +32,7 @@ wire bIRQ_n;
 assign nData = bRead ? 8'bz : nWriteData;
 
 VIA_6522 UUT (
-	.clk(clk),
+	.bPhase2Clock(bPhase2Clock),
 	.bReset_n(bReset_n),
 	.bCS(bCS),
 	.bCS_n(bCS_n),
@@ -45,84 +45,106 @@ VIA_6522 UUT (
 initial begin
 	$dumpvars(0, VIA_6522_TB);
 
-	#16 bReset_n = 1;				// Release VIA From Reset
-	#9 bCS = 1;
-	#2
+	#10 bReset_n = 1;				// Release VIA From Reset
+	#22
 
-	#2 bCS_n = 0;					// Enable Interrupts VIA_IER_CA2, VIA_IER_SR and VIA_IER_T1 0x45
+	#6 bCS = 1;
+	bCS_n = 0;
 	nRS = VIA_REG_IER;
 	nWriteData = 8'hC5;
-	#1 bRead = 0;
-	#6 bRead = 1;
+	bRead = 0;
+	#10 bCS = 0;
 	bCS_n = 1;
+	bRead = 1;
 
-	#2 bCS_n = 0;					// Disable Interrupts VIA_IER_CA2, VIA_IER_SR 0x40
+	#6 bCS = 1;						// Disable Interrupts VIA_IER_CA2, VIA_IER_SR 0x40
+	bCS_n = 0;
 	nRS = VIA_REG_IER;
 	nWriteData = 8'h05;
-	#1 bRead = 0;
-	#6 bRead = 1;
+	bRead = 0;
+	#10 bCS = 0;
 	bCS_n = 1;
+	bRead = 1;
 
 `ifdef TEST_TIMER_1
-	#2 bCS_n = 0;					// Write 0x40 To ACR Putting Timer 1 Into Free Running Mode
+	#6 bCS = 1;						// Write 0x40 To ACR Putting Timer 1 Into Free Running Mode
+	bCS_n = 0;
 	nRS = VIA_REG_ACR;
 	nWriteData = 8'h40;
-	#1 bRead = 0;
-	#6 bRead = 1;
+	bRead = 0;
+	#10 bCS = 0;
 	bCS_n = 1;
+	bRead = 1;
 
-	#2 bCS_n = 0;					// Write 0x26 To Timer 1 Low Order Latch Through Count Register
+	#6 bCS = 1;						// Write 0x26 To Timer 1 Low Order Latch Through Count Register
+	bCS_n = 0;
 	nRS = VIA_REG_T1CL;
 	nWriteData = 8'h06;		// 26
-	#1 bRead = 0;
-	#6 bRead = 1;
+	bRead = 0;
+	#10 bCS = 0;
 	bCS_n = 1;
+	bRead = 1;
 
-	#2 bCS_n = 0;					// Write 0x48 To Timer 1 High Order Latch And Start The Timer
+	#6 bCS = 1;						// Write 0x48 To Timer 1 High Order Latch And Start The Timer
+	bCS_n = 0;
 	nRS = VIA_REG_T1CH;
 	nWriteData = 8'h00;		// 48
-	#1 bRead = 0;
-	#6 bRead = 1;
+	bRead = 0;
+	#10 bCS = 0;
 	bCS_n = 1;
+	bRead = 1;
 
-	#65								// Read Interrupt Flags Register
+	#96								// Count Down 6
+	#16								// IRQ Set Next Clock - Very This On Real Hardware
+
+	#6 bCS = 1;						// Read Interrupt Flags Register
+	bCS_n = 0;
 	nRS = VIA_REG_IFR;
-	#1 bCS_n = 0;
-	#7 bCS_n = 1;
+	#10 bCS = 0;
+	bCS_n = 1;
 
 	if (nData != 8'hC0)
 		$error("Interrupt Flag Error!");
 
 	if (bIRQ_n != 0)
 		$error("IRQ Set Flag Error!");
-/*
-	nWriteData = 8'h40;				// Reset IRQ By Clearing The Timer 1 IFR Bit
-	#2 bRead = 0;
-	bCS_n = 0;
-	#7 bCS_n = 1;
 
-	nWriteData = 8'h00;				// Reset IRQ By ReWriting Timer 1 High Order Latch
-	nRS = VIA_REG_T1LH;
-	#2 bRead = 0;
+	// #6 bCS = 1;						// Reset IRQ By Clearing The Timer 1 IFR Bit
+	// bCS_n = 0;
+	// nRS = VIA_REG_IFR;
+	// nWriteData = 8'h40;
+	// bRead = 0;
+	// #10 bCS = 0;
+	// bCS_n = 1;
+	// bRead = 1;
+
+	// #6 bCS = 1;						// Reset IRQ By ReWriting Timer 1 High Order Latch
+	// bCS_n = 0;
+	// nRS = VIA_REG_T1LH;
+	// nWriteData = 8'h00;		// 48
+	// bRead = 0;
+	// #10 bCS = 0;
+	// bCS_n = 1;
+	// bRead = 1;
+
+	#6 bCS = 1;						// Reset IRQ By Reading Timer 1 Low Order Counter
 	bCS_n = 0;
-	#7 bCS_n = 1;
-*/
-	nRS = VIA_REG_T1CL;				// Reset IRQ By Reading Timer 1 Low Order Counter
-	#2 bCS_n = 0;
-	#7 bCS_n = 1;
+	nRS = VIA_REG_T1CL;
+	#10 bCS = 0;
+	bCS_n = 1;
 
 	if (bIRQ_n != 1)
 		$error("IRQ Reset Flag Error!");
 
-	#20 nRS = VIA_REG_ACR;
-	nWriteData = 8'h00;				// Switch Timer 1 Into Single Shot Mode
+	#6 bCS = 1;						// Switch Timer 1 Into Single Shot Mode
+	bCS_n = 0;
+	nRS = VIA_REG_ACR;
+	nWriteData = 8'h00;
 	bRead = 0;
-	#1 bCS_n = 0;
-	#6 bRead = 1;
+	#10 bCS = 0;
 	bCS_n = 1;
-
+	bRead = 1;
 `endif // TEST_TIMER_1
-
 
 	#(DURATION) $display("End of simulation");
 	$finish;
